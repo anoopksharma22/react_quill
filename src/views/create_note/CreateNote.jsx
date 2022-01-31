2186cd6e865b3b6eb7fece9fcf1c56b3a451d562
Anoop Sharma <anoopksharma22@gmail.com>
2022-02-01 03:21:35 +0530

    Added katex for formula

--- src/views/create_note/CreateNote.jsx
+++ src/views/create_note/CreateNote.jsx
@@ -3,40 +3,16 @@ import "./create_note.css";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 import BlotFormatter from "quill-blot-formatter";
+import { options } from "./editor_options";
+import "katex";
+import katex from "katex";
+import "katex/dist/katex.min.css";
 
 const CreateNote = () => {
   const wraperRef = useCallback((wraper) => {
     if (wraper == null) return;
     wraper.innerHTML = "";
-    const toolbarOptions = [
-      ["bold", "italic", "underline", "strike"], // toggled buttons
-      ["blockquote", "code-block"],
-
-      [{ header: 1 }, { header: 2 }], // custom button values
-      [{ list: "ordered" }, { list: "bullet" }],
-      [{ script: "sub" }, { script: "super" }], // superscript/subscript
-      [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
-      [{ direction: "rtl" }], // text direction
-
-      [{ size: ["small", false, "large", "huge"] }], // custom dropdown
-      [{ header: [1, 2, 3, 4, 5, 6, false] }],
-
-      [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-      [{ font: [] }],
-      [{ align: [] }],
-      ["image", "video"],
-
-      ["clean"], // remove formatting button
-    ];
-    var options = {
-      debug: "info",
-      placeholder: "Compose an epic...",
-      theme: "snow",
-      modules: {
-        toolbar: toolbarOptions,
-        blotFormatter: {},
-      },
-    };
+    window.katex = katex;
     const editor = document.createElement("div");
     wraper.append(editor);
     Quill.register("modules/blotFormatter", BlotFormatter);
