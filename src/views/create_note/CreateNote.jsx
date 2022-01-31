0da631c156b3da95b38af8441d2782817517d5b3
Anoop Sharma <anoopksharma22@gmail.com>
2022-02-01 03:53:36 +0530

    Added logic to get content on button click

--- src/views/create_note/CreateNote.jsx
+++ src/views/create_note/CreateNote.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import "./create_note.css";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
@@ -9,6 +9,7 @@ import katex from "katex";
 import "katex/dist/katex.min.css";
 
 const CreateNote = () => {
+  const [quill, setQuill] = useState();
   const wraperRef = useCallback((wraper) => {
     if (wraper == null) return;
     wraper.innerHTML = "";
@@ -16,12 +17,17 @@ const CreateNote = () => {
     const editor = document.createElement("div");
     wraper.append(editor);
     Quill.register("modules/blotFormatter", BlotFormatter);
-    new Quill(editor, options);
+    const q = new Quill(editor, options);
+    setQuill(q);
   }, []);
 
+  const saveContent = () => {
+    console.log(quill.getContents());
+  };
   return (
     <>
       <div id="container" ref={wraperRef}></div>
+      <button onClick={saveContent}>SaveContent</button>
     </>
   );
 };
